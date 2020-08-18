6d92bb716a9861fdc2d24246e411965b2b851eb8
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-08-18 23:08:23 +0000

    v1

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Digite dois números: ");
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Soma é: " + (n1 + n2));
   }
 }
